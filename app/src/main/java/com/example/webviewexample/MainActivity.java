dac16fb12e3ba15d7e4a5ca3cfb8affac24d02ab
Mahmud Hasan Jion <mahmudh.jion02@gmail.com>
2023-07-06 10:32:02 +0100

    update setContentView parameter

--- app/src/main/java/com/example/webviewexample/MainActivity.java
+++ app/src/main/java/com/example/webviewexample/MainActivity.java
@@ -1,12 +1,10 @@
 package com.example.webviewexample;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-
 
 public class MainActivity extends AppCompatActivity {
     private WebView webView;
@@ -17,8 +15,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setContentView(R.layout.activity_main);
-        webView = (WebView) findViewById(R.id.webview);
+
+        webView = findViewById(R.id.webview);
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl("https://www.google.com/");
         webView.getSettings().setUserAgentString(USER_AGENT);
